63223edb8f9f9fb861a1397018acdc9ed2c60304
beavicario <bea.vg.9363@gmail.com>
2021-04-12 14:34:33 +0200

    S6

---
+++ TEORÍA/S6/canvas-07.js
@@ -0,0 +1,21 @@
+
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del canvas
+canvas.width = 1300;
+canvas.height = 500;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Leer la imagen del documento html
+//-- Esta deshabilitada
+var logo = document.getElementById("logo-urjc");
+
+logo.onload = ()=> {
+  //-- Insertar la imagen en el canvas, una vez que
+  //-- ya esté cargada!
+  ctx.drawImage(logo, 15,18);
+};
